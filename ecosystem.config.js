e1b04c9ef6b6391b790c7e13696839cfdb99ea5b
Rahul <69046288+nxious@users.noreply.github.com>
2020-11-30 15:21:19 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,11 +7,11 @@ module.exports = {
       },
       env_production: {
         NODE_ENV: "production",
-        MYSQL_USER="root",
-        MYSQL_DATABASE="dvna",
-        MYSQL_PASSWORD="passw0rd",
-        MYSQL_HOST="127.0.0.1",
-        MYSQL_PORT="3306",
+        MYSQL_USER:"root",
+        MYSQL_DATABASE:"dvna",
+        MYSQL_PASSWORD:"passw0rd",
+        MYSQL_HOST:"127.0.0.1",
+        MYSQL_PORT:"3306",
       }
     }]
   }
