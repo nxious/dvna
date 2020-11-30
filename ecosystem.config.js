89ee8220c285ee88d593d0cc956d17a26e0a8f0b
Rahul <69046288+nxious@users.noreply.github.com>
2020-11-30 15:17:47 +0530

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    apps : [{
+      name: "DVNA",
+      script: "server.js",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+        MYSQL_USER="root",
+        MYSQL_DATABASE="dvna",
+        MYSQL_PASSWORD="passw0rd",
+        MYSQL_HOST="127.0.0.1",
+        MYSQL_PORT="3306",
+      }
+    }]
+  }
+  
